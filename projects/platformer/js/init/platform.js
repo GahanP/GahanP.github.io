cb641335c55cdf8c77bbcb75861bcc6eca23d1cf
GahanP <patelgahan2006@gmail.com>
2021-06-11 03:05:43 +0000

    add cannons to game

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -36,9 +36,10 @@ y: The y coordinate for the platform.
 scaleX: OPTIONAL The scale factor on the x-axis, this value will stretch the platform in width.
 scaleY: OPTIONAL The scale factor on the y-axis, this value will stretch the platform in height.
 */
-createPlatform(400, 460);           // normal platform
-createPlatform(300, 200, 0.3)       // small horizontal platform (30% the normal width)
+createPlatform(450, 250);           // normal platform
+createPlatform(300, 300, 0.2)       // small horizontal platform (30% the normal width)
 createPlatform(500, 500, 0.3, 10)   // tall vertical wall (30% the normal width and 10x the height)
+createPlatform(150, 400);
 
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
