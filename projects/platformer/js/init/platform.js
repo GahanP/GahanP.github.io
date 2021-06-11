adcab3018ac08bf0262bfd72a5bbd4f9f202c563
GahanP <patelgahan2006@gmail.com>
2021-06-11 02:48:13 +0000

    add platforms and collectables to game

---
+++ projects/platformer/js/init/platform.js
@@ -0,0 +1,47 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.platform = window.opspark.platform || {};
+    
+    let platform = window.opspark.platform;
+    
+    /**
+     * init: This function initializes the platforms for the level.
+     * 
+     * GOAL: Add as many platforms necessary to make your level challenging.
+     * 
+     * Use the createPlatform Function to create platforms for the level. 
+     * 
+     * createPlatform() takes these arguments:
+     *      
+     *      createPlatform(x, y, scaleX, scaleY);
+     * 
+     *      x: The x coordineate for the platform.
+     *      y: The y coordineate for the platform.
+     *      scaleX: OPTIONAL The scale factor on the x-axis, this value will 
+     *              stretch the platform in width.
+     *      scaleY: OPTIONAL The scale factor on the y-axis, this value will 
+     *              stretch the platform in height.
+     */ 
+    function init(game) {
+        let createPlatform = platform.create;
+    
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        /*
+createPlatform(x, y, scaleX, scaleY);
+ 
+x: The x coordinate for the platform.
+y: The y coordinate for the platform.
+scaleX: OPTIONAL The scale factor on the x-axis, this value will stretch the platform in width.
+scaleY: OPTIONAL The scale factor on the y-axis, this value will stretch the platform in height.
+*/
+createPlatform(400, 460);           // normal platform
+createPlatform(300, 200, 0.3)       // small horizontal platform (30% the normal width)
+createPlatform(500, 500, 0.3, 10)   // tall vertical wall (30% the normal width and 10x the height)
+
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    }
+    platform.init = init;
+})(window);
