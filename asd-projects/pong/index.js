321c8d92bbeae68378098934e418f76b0be49897
GahanP <patelgahan2006@gmail.com>
2022-06-27 14:29:48 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -12,7 +12,12 @@ function runProgram(){
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
   // Game Item Objects
-
+function left(id) {
+  var leftPaddle = {};
+  leftPaddle.id = id
+  leftPaddle.x = parseFloat($(id).css('left'));
+  leftPaddle.y = parseFloat($(id).css('top'));
+}
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
@@ -53,6 +58,10 @@ function runProgram(){
     task.speedY = 0;
     return task;
   }
+factory(#ball);
+factory(#leftPaddle);
+factory(#rightPaddle);
+
   
   function endGame() {
     // stop the interval timer
