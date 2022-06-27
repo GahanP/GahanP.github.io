9b2e13ea3bb1acacd7b12c1683f4449b9d8431be
GahanP <patelgahan2006@gmail.com>
2022-06-27 16:06:43 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -12,8 +12,10 @@ function runProgram() {
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
 
   var KEYCODE = {
-    W: 87,
-    S: 83,
+    "W": 87,
+    "S": 83,
+    "UP":38,
+    "DOWN":40,
   }
 
 
@@ -25,9 +27,11 @@ function runProgram() {
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
   $(document).on('keyup', handleKeyUp);    
-  $(document).on('keydown', handleKeyDown);                                         // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);           
+  $(document).on('keydown', handleKeyDownRight);     
+  $(document).on('keyup', handleKeyUpRight);                                   // change 'eventType' to the type of event you want to handle
 
-  ////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////s/////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
@@ -51,11 +55,11 @@ function runProgram() {
       leftPaddle.speedY = -5;
     }
   }
-  function handleKeyDown(event) {
-    if (event.which === KEY.W) {
+  function handleKeyDownRight(event) {
+    if (event.which === KEY.UP) {
       rightPaddle.speedY = +5;
     }
-    if (event.which === KEY.S) {
+    if (event.which === KEY.DOWNgit add -A) {
       rightPaddle.speedY = -5;
     }
   }
@@ -67,11 +71,11 @@ function runProgram() {
       leftPaddle.speedY = 0;
     }
   }
-  function handleKeyUp(event) {
-    if (event.which === KEY.W) {
+  function handleKeyUpRight(event) {
+    if (event.which === KEY.UP) {
       rightPaddle.speedY = 0;
     }
-    if (event.which === KEY.S) {
+    if (event.which === KEY.DOWN) {
       rightPaddle.speedY = 0;
     }
   }
