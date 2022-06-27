0fbb4af796f2e675a47f47d83b8fcaaff2b25577
GahanP <patelgahan2006@gmail.com>
2022-06-27 15:54:15 +0000

    saving pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -1,8 +1,8 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
-function runProgram(){
+
+function runProgram() {
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
@@ -10,21 +10,22 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  
+
   var KEYCODE = {
     W: 87,
     S: 83,
   }
-  
-  }
+
+
   // Game Item Objects
-var ball = factory("#ball");
-var leftPaddle = factory("#lefPaddle");
-var rightPaddle = factory("#rightPaddle");
+  var ball = factory("#ball");
+  var leftPaddle = factory("#lefPaddle");
+  var rightPaddle = factory("#rightPaddle");
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keyup', handleKeyUp);    
+  $(document).on('keydown', handleKeyDown);                                         // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -35,10 +36,10 @@ var rightPaddle = factory("#rightPaddle");
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
+
 
   }
-  
+
   /* 
   Called in response to events.
   */
@@ -46,23 +47,24 @@ var rightPaddle = factory("#rightPaddle");
     if (event.which === KEY.W) {
       speedY = +5;
     }
-    if (event,which === KEY.S) {
+    if (event, which === KEY.S) {
       speedY = -5;
     }
-    function handleKeyUp(event) {
-      if (event.which === KEY.W) {
-        speedY = 0;
-      }
-      if (event.which === KEY.S) {
-        speedY = 0;
-      }     
+  }
+  function handleKeyUp(event) {
+    if (event.which === KEY.W) {
+      speedY = 0;
+    }
+    if (event.which === KEY.S) {
+      speedY = 0;
+    }
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
-  function factory(id){
+  function factory(id) {
     var task = {};
     task.id = id
     task.x = parseFloat($(id).css('left'));
@@ -74,7 +76,7 @@ var rightPaddle = factory("#rightPaddle");
     return task;
   }
 
-  
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
